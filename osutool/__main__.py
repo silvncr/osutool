d305a024f0abfb1ea064939278bdbe65c6921ad4
silvncr <johnsdieckmann@gmail.com>
2024-07-26 10:19:34 +1000

    fix text and formatting

--- osutool/__main__.py
+++ osutool/__main__.py
@@ -1,11 +1,3 @@
-'''
-Main entrypoint for the osutool package, executed when the package is run as a script.
-
-```sh
-$ python -m osutool
-```
-'''
-
 from osutool import main
 
 if __name__ == '__main__':
